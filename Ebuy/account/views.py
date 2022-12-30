3b30bb7237abb50212e6910a32264f705cab1f3f
Hao Tang <61295938+haotangphuoc@users.noreply.github.com>
2022-12-29 20:56:50 -0400

    Create authentication for user

--- Ebuy/account/views.py
+++ Ebuy/account/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 from django.shortcuts import render
@@ -6,4 +10,27 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "account/index.html")
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("account:login"))
+    return render(request, "account/user.html")
+
+def login_view(request):
+    if request.method == "POST":
+        # Access and authenticate user
+        username = request.POST["username"]
+        password = request.POST["password"]
+        
+        user = authenticate(request, username=username, password=password)
+        
+        #If user is returned, log in, else, return login page
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(reverse("account:index"))
+        else:
+            return render(request, "account/login.html", {
+                "message": "Invalid email or password, try again"
+            })
+    return render(request, "account/login.html")
+
+def logout_view(request):
+    pass
