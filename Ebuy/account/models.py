697ccf581b58f9b08b7febe37d03779aabd2f630
Hao Tang <61295938+haotangphuoc@users.noreply.github.com>
2022-12-30 14:51:26 -0400

    Add user registration

--- Ebuy/account/models.py
+++ Ebuy/account/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    address = models.CharField(max_length=1024)
+    #Can be improved
+    city = models.CharField(max_length=50, default="Halifax")
+    province = models.CharField(max_length=2, default="NS")
+    
