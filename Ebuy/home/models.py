5c2ba6d32404968a7b8187a298f5c02b7e50197e
Eduard Kakosyan <90533005+EduardKakosyan@users.noreply.github.com>
2023-01-16 10:08:01 -0400

    Created an sql form for product selling

--- Ebuy/home/models.py
+++ Ebuy/home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from account import UserProfile
+
 
 # Create your models here.
+class ProductProfile(models.Model):
+    productName = models.OneToOneField(User, on_delete = models.CASCADE, related_name = "product_profile")
+    price = models.DecimalField(max_digits = 10, decimal_places = 2)
+    date = models.DateTimeField()
+    seller = models.ForeignKey(UserProfile, on_delete = models.CASCADE, related_name = "user_profile")
