d7d6251d15357b31ea7ef2d26bbff740122b3410
Hao Tang <61295938+haotangphuoc@users.noreply.github.com>
2023-05-17 10:54:31 -0300

    Fix products display layout and add image field to the product

--- Ebuy/account/admin.py
+++ Ebuy/account/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import *
 
 # Register your models here.
-admin.site.register(UserProfile)
+
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
--- Ebuy/home/views.py
+++ Ebuy/home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from account.models import *
 
 # Create your views here.
 def index(request):
-    return render(request, "home/index.html")
+    products = Product.objects.all()
+    fields = {"products": products}
+    return render(request, "home/index.html", fields)
