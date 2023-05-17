9161302d5dc73bc428b9bf3f727b1f2a9d49b980
Hao Tang <61295938+haotangphuoc@users.noreply.github.com>
2023-05-17 16:31:53 -0300

    Altered models structure

--- Ebuy/account/models.py
+++ Ebuy/account/models.py
@@ -5,8 +5,13 @@ from uuid import uuid4
 def generateUUID():
     return str(uuid4())
 
+class Order(models.Model):
+    complete = models.BooleanField(default=False, blank=False)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    
 class Product(models.Model):
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, related_name="products")
+    order = models.ForeignKey(Order, null=True, blank=True, on_delete=models.CASCADE, related_name="products")
     name = models.CharField(max_length=100)
     price = models.FloatField()
     image = models.ImageField(null=True, blank=True, upload_to="static/images")
@@ -22,33 +27,13 @@ class Product(models.Model):
         except:
             url = ''
         return url
-    
-    
-class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
-    date_ordered = models.DateTimeField(auto_now_add=True)
-    complete = models.BooleanField(default=False, blank=False)
-    transaction_id = models.CharField(max_length=200, null=True)
-    
-    def _str_(self):
-        return str(self.id)
-
-
-class OrderItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
-    quantity = models.IntegerField(default=0, null=True, blank=True)
-    date_ordered = models.DateTimeField(auto_now_add=True)
-    
 
 class ShippingAddress(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     address = models.CharField(max_length=200, null=True)
     city = models.CharField(max_length=200, null=True)
     state = models.CharField(max_length=200, null=True)
     zipcode = models.CharField(max_length=200, null=True)
-    date_ordered = models.DateTimeField(auto_now_add=True)
     
     def _str_(self):
         return str(self.id)
